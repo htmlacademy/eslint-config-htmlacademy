906441039e78ea597d8ffb6ad63173e032e25911
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-11-04 00:48:06 +0300

    fixed #1 moved to newer ESLint version

--- es5.js
+++ es5.js
@@ -98,7 +98,15 @@ module.exports = {
         // Stylistic Issues
         // http://eslint.org/docs/rules/#stylistic-issues
         // ----------------------------------------------
-        'indent': ['error', 2, {SwitchCase: 1, VariableDeclarator: 2, MemberExpression: 2}],
+        'indent': ['error', 2, {
+            SwitchCase: 1,
+            // continuation indent
+            VariableDeclarator: 2,
+            MemberExpression: 2,
+            FunctionDeclaration: {parameters: 2},
+            FunctionExpression: {parameters: 2},
+            CallExpression: {arguments: 2}
+        }],
         'block-spacing': ['error', 'always'],
         'array-bracket-spacing': ['error', 'never'],
         'brace-style': 'error',
