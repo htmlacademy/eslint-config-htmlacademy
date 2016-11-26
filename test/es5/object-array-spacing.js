1d3b0b223118d4d49e3224be2b5a463487c1b2fd
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2016-11-26 22:08:03 +0300

    fix #3 object array spacing and destructuring spacing tests

---
+++ test/es5/object-array-spacing.js
@@ -0,0 +1,7 @@
+'use strict';
+
+// http://eslint.org/docs/rules/array-bracket-spacing.html
+// http://eslint.org/docs/rules/object-curly-spacing.html
+var array = [1, 2, 3];
+var object = {a: 1, b: 2};
+throw new Error(array, object);
