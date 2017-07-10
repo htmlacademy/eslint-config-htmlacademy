ab7964bd61b5802d46728f93ea5548539cabc2d1
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2017-07-10 19:14:45 +0300

    closes #12

---
+++ test/es6/no-empty.js
@@ -0,0 +1,13 @@
+const client = {
+  emit(message, data) {
+    return `${message} and ${data}`;
+  }
+};
+
+const data = {};
+
+try {
+  client.emit(`message`, JSON.stringify(data));
+} catch (ignore) {
+  // ignore it
+}
