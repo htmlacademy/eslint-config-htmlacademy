a6fe3ce30c35c77e2738221010ecf9b637113566
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-04-04 14:23:07 +0300

    update tests

---
+++ test/es5/no-undefined.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var myUndefined = 1;
+
+myUndefined = undefined;
+
+throw new Error(myUndefined);
--- test/es6/.eslintrc.js
+++ test/es6/.eslintrc.js
@@ -1,4 +1,13 @@
 const config = require('../../es6');
+const es5config = require('../../es5');
+
+const es5Rules = es5config.rules;
+
+config.rules = {
+  ...config.rules,
+  ...es5Rules
+};
+
 config.parserOptions = {
   ecmaVersion: 6,
   sourceType: 'module'
