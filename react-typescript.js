d4115ebff6eeaa1259eb636ede7e8359371b8e4f
Igor Antonov <antonov.igor.khv@gmail.com>
2021-09-28 20:24:36 +0300

    Added rules to exclude import react in scope (#69)

--- react-typescript.js
+++ react-typescript.js
@@ -4,6 +4,8 @@ module.exports = {
     'plugin:@typescript-eslint/recommended'
   ],
   rules: {
+    'react/jsx-uses-react': 'off',
+    'react/react-in-jsx-scope': 'off',
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': ['error'],
     'no-shadow': 'off',
