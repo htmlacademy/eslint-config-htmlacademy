7ef3f0258a985889b8fb85e452202be78e81de7a
kam4atka <sledsib@ya.ru>
2022-03-24 18:56:29 +0300

    Added rules for config of Node

--- node.js
+++ node.js
@@ -1,11 +1,47 @@
 module.exports = {
-    extends: [`htmlacademy/es6`],
+    extends: [
+      'htmlacademy/vanilla',
+      'plugin:@typescript-eslint/recommended',
+      'plugin:node/recommended',
+    ],
     rules: {
-      // Node.js
-      // Specific overridden rules
+      // Possible Errors
       // ------------------------------------------
-      'no-console': `off`,
-      'no-process-exit': `off`
+      'no-console': 'off',
+      'node/handle-callback-err': ['error', "^.*(e|E)rr"],
+      'node/no-callback-literal': 'error',
+      'node/no-exports-assign': 'off',
+      'node/no-extraneous-import': 'off',
+      'node/no-extraneous-require': 'off',
+      'node/no-missing-import': 'off',
+      'node/no-missing-require': 'off',
+      'node/no-new-require': 'off',
+      'node/no-path-concat': 'error',
+      'node/no-process-exit': 'error',
+      'node/no-unpublished-bin': 'off',
+      'node/no-unpublished-import': 'off',
+      'node/no-unpublished-require': 'off',
+      'node/no-unsupported-features/es-builtins': 'off',
+      'node/no-unsupported-features/node-builtins': 'off',
+      'node/process-exit-as-throw': 'error',
+      'node/shebang': 'off',
+      // Stylistic Issues
+      // ------------------------------------------
+      'node/callback-return': 'error',
+      'node/exports-style': 'off',
+      'node/file-extension-in-import': ['error', 'always'],
+      'node/global-require': 'error',
+      'node/no-mixed-requires': 'error',
+      'node/no-process-env': 'off',
+      'node/no-restricted-import': 'off',
+      'node/no-restricted-require': 'off',
+      'node/no-sync': 'off',
+      'node/prefer-global/buffer': 'error',
+      'node/prefer-global/text-decoder': 'error',
+      'node/prefer-global/text-encoder': 'error',
+      'node/prefer-global/url-search-params': 'error',
+      'node/prefer-global/url': 'error',
+      'node/prefer-promises/dns': 'error',
+      'node/prefer-promises/fs': 'error',
     }
   };
-  
